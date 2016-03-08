0527f93b12f2e2a3e62da88829ab077336f16f6a
mzennis <meytajennist@gmail.com>
2016-03-08 13:52:14 +0700

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-DocoLibs_Example/Pods-DocoLibs_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DocoLibs_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DocoLibs_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-DocoLibs_Tests/Pods-DocoLibs_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DocoLibs_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DocoLibs_TestsVersionString[];
+
